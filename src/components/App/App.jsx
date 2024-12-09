c572afe15db65b8ccd4417819ddd12752d26e10a
Dakodah Spurlin <dcspurlin@gmail.com>
2024-12-09 12:16:36 -0600

    Template set up with add function.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,63 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import TableUpdate from '../TableUpdate/TableUpdate';
 
 function App () {
+
+  const [ newToDo, setNewToDo ] = useState( '' );
+  const [ toDos, setToDos ] = useState( [] );
   
+function addToDo(  ) {
+  
+  console.log('new To Do: ', newToDo)
+  setToDos( [...toDos, newToDo] )
+  console.log('The list so far: ', toDos)
+
+}
+
   return (
     <div>
-      <h1>TO DO APP</h1>
+      <h1>Your TO DO List</h1>
+      <input type='text' placeholder='Add To Do' size={40} onChange={(event)=>{setNewToDo(event.target.value)}}/>
+      <br />
+      <p>{newToDo}</p>
+      <br />
+      <button onClick={ addToDo }>Add</button>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>To Do Item</th>
+            <th>Completed?</th>
+            <th>Delete?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Fuck Amazons mom</td>
+            <td>No</td>
+            <td>
+              <button>Remove</button>
+            </td>
+          </tr>
+
+          </tbody>
+      </table>
+          <ul>
+          { toDos.map( (toDos, index) => (
+        <li key={index}> 
+        { toDos } 
+        <button>❌</button> 
+        <button>Remove</button>
+        </li>
+      ))
+      
+      }
+          </ul>
+
+          {/* <TableUpdate toDos={ toDos } /> */}
+          
+        
+
     </div>
   );
 
